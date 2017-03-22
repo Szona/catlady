67617d4a0863c0bf12461efc02daf057b3e35649
Alicja Ćmiel <cmielalicja@gmail.com>
2017-03-22 14:21:24 +0000

    files set-up

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+//var browserSync = require('browser-sync').create();
+var minify = require('gulp-minify');
+
+gulp.task('sass', function() {
+  return gulp.src('scss/style.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+      outputStyle: 'expanded', //nested compact compressed expanded
+      errLogToConsole: true}))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', ['sass'], function(){
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
