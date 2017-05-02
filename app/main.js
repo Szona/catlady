2bfd4fee14ee77c4e597bc85f51622f5afcb9411
Alicja Ćmiel <cmielalicja@gmail.com>
2017-05-02 16:48:37 +0100

    scroll animation

--- app/main.js
+++ app/main.js
@@ -1,6 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var load = getElementsByClassName('loader-wrapp');
+  var load = document.getElementsByClassName("loader-wrapper");
   console.log(load);
+  var body = document.querySelector("body");
+
+  setTimeout(function() {
+    load[0].style.display="none";
+    body.style.overflow="initial"}, 4100);
+
+    //slider on the monitor
+
+    const imgList = document.querySelectorAll("#slider li");
+    console.log(imgList);
+
+
+
+
 
 });
