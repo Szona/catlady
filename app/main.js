d2a26ca7ffe7b712c1b7952bbd724b96b9ef76c4
Alicja Ćmiel <cmielalicja@gmail.com>
2017-05-03 12:20:10 +0100

    nav bar

--- app/main.js
+++ app/main.js
@@ -10,8 +10,31 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //slider on the monitor
 
-    const imgList = document.querySelectorAll("#slider li");
-    console.log(imgList);
+  //hamburger menu
+
+  const imgList = document.querySelectorAll("#slider li");
+  console.log(imgList);
+
+  const hamburger = document.querySelector(".hamburger");
+  console.log(hamburger);
+  const lines = document.querySelectorAll(".hamburger span");
+  console.log(lines);
+  const li = document.querySelectorAll(".menu li");
+
+  hamburger.addEventListener("click", function(e){
+  console.log("click");
+
+  lines[0].classList.toggle("hide");
+  lines[1].classList.toggle("left");
+  lines[2].classList.toggle("right");
+
+  for (var i = 0; i < li.length; i++) {
+    li[i].classList.toggle("visible");
+  };
+
+
+});
+
 
 
 
