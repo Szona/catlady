863100551d8f627c5fafc7dab07b46311373e823
Alicja Ćmiel <cmielalicja@gmail.com>
2017-05-03 14:40:58 +0100

    slider

--- app/main.js
+++ app/main.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var load = document.getElementsByClassName("loader-wrapper");
-  console.log(load);
-  var body = document.querySelector("body");
+  const load = document.getElementsByClassName("loader-wrapper");
+  const body = document.querySelector("body");
 
   setTimeout(function() {
     load[0].style.display="none";
@@ -10,25 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
     //slider on the monitor
 
+
   //hamburger menu
 
-  const imgList = document.querySelectorAll("#slider li");
-  console.log(imgList);
-
   const hamburger = document.querySelector(".hamburger");
-  console.log(hamburger);
+  // console.log(hamburger);
   const lines = document.querySelectorAll(".hamburger span");
-  console.log(lines);
+  // console.log(lines);
   const li = document.querySelectorAll(".menu li");
 
   hamburger.addEventListener("click", function(e){
-  console.log("click");
+  // console.log("click");
 
   lines[0].classList.toggle("hide");
   lines[1].classList.toggle("left");
   lines[2].classList.toggle("right");
 
-  for (var i = 0; i < li.length; i++) {
+  for (let i = 0; i < li.length; i++) {
     li[i].classList.toggle("visible");
   };
 
